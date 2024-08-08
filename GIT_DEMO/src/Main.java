a5681934b7abdb56ee8af2f9389ac49e209d8aaa
Musabbereen <coclans6967@gmail.com>
2024-08-09 02:21:53 +0600

    2nd commit

--- GIT_DEMO/src/Main.java
+++ GIT_DEMO/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is done successfully");
         System.out.println("dasdasdad");
+        System.out.println("Why was it not working in the beginning ");
     }
 }
