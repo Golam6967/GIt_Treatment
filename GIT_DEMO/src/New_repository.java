e4acbc06ade240ff25f7576b5ce3ad5a64234218
Golam6967 <coclans6982@gmail.com>
2024-08-09 17:21:15 +0600

    Update New_repository.java

--- GIT_DEMO/src/New_repository.java
+++ GIT_DEMO/src/New_repository.java
@@ -1,5 +1,6 @@
 public class New_repository {
     public static void main(String[] args) {
         System.out.println("Now I will do something that will fix my error");
+          System.out.println("Now I will do something that will fix my error");
     }
 }
