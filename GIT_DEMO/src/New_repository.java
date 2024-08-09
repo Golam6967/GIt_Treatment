8bba73e630ce76b0ebc202c6415b769277c0763b
Musabbereen <coclans6967@gmail.com>
2024-08-09 17:25:15 +0600

    Version 467

--- GIT_DEMO/src/New_repository.java
+++ GIT_DEMO/src/New_repository.java
@@ -2,5 +2,6 @@ public class New_repository {
     public static void main(String[] args) {
         System.out.println("Now I will do something that will fix my error");
           System.out.println("Now I will do something that will fix my error");
+        System.out.println("So mate I am able to do anything with my code and I can solve any problem that I have been facing during learnming the git and git commands");
     }
 }
